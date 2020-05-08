12a0ed709f9242d1b5010e612414583f8087a003
jose manuel muñoz <josema.munoz@gmail.com>
2020-05-08 13:56:37 -0400

    Se suben los archivos

---
+++ excuse.js
@@ -0,0 +1,7 @@
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+let excuse = who[Math.floor(Math.random()*who.length)] + " " + what[Math.floor(Math.random()*what.length)] + " " + when[Math.floor(Math.random()*when.length)] ;
+
+document.write(excuse);
